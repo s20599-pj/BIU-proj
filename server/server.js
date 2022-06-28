3203347b06a6779eaa0e71bcddf19ff2c02ff563
Rominski, Kamil <s20599@pjwstk.edu.pl>
2022-06-29 01:18:31 +0200

    Added api call to add comment

--- server/server.js
+++ server/server.js
@@ -6,15 +6,20 @@ const comments = require("./data/comments.json");
 const coctails = require("./data/coctails.json");
 const server = http.createServer(app);
 const fs = require("fs");
+const bodyParser = require('body-parser')
+
+const COMMENTS_PATH = "./data/comments.json";
+const COCTAILS_PATH = "./data/coctails.json";
 
 const port = 3001;
 require("dotenv").config();
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+console.log(comments);
 
 app.get("/api/getCoctails", (req, res) => {
    res.send(coctails);
@@ -27,5 +32,33 @@ app.get("/api/getComments", (req, res) => {
 app.get("/api/getAll", (req, res) => {
     let response = {coctail: coctails, comments: comments};
     res.send(response);
-})
+});
+
+app.post("/api/saveComment", (req, res) => {
+    const data = req.body;
+    console.log(data);
+
+    const newComment = {
+        "id": comments[comments.length-1].id+1,
+        "coctail_id": data.coctail_id,
+        "author": data.author,
+        "date": getDate(),
+        "description": data.description
+    }
+    console.log(newComment);
+    comments.push(newComment);
+    console.log(comments);
+    let toSave = JSON.stringify(comments,null,2);
+    fs.writeFileSync(COMMENTS_PATH, toSave);
+    res.send(toSave);
+});
+
+const getDate = () => {
+    let today = new Date();
+    let mm = today.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
+    let date = today.getDate() + '-' + mm + '-' + today.getFullYear();
+    return date.toString();
+}
+
 server.listen(port, () => console.log(`Listening on port 3001`));
